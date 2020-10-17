d6adacc5fa7dea78eb09fa8ec7594039c7fa0d21
Mattèo Gauthier <matteo.gauthier@gmail.com>
2020-10-17 10:12:42 +0200

    Adding experiments directory, tailwind ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,66 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
 module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
   purge: ["./src/*.html"],
+  experimental: {
+    uniformColorPalette: true,
+    applyComplexClasses: true,
+    extendedSpacingScale: true,
+    defaultLineHeights: true,
+    extendedFontSizeScale: true,
+  },
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["'Inter'", "Inter", ...defaultTheme.fontFamily.sans],
+    },
+    extend: {
+      borderWidth: {
+        1: "0.5px",
+      },
+      screens: {},
+      maxWidth: {
+        "1/2": "50%",
+        "1/3": "33.333333%",
+        "2/3": "66.666667%",
+        "1/4": "25%",
+        "2/4": "50%",
+        "3/4": "75%",
+        "1/5": "20%",
+        "2/5": "40%",
+        "3/5": "60%",
+        "4/5": "80%",
+        "1/6": "16.666667%",
+        "2/6": "33.333333%",
+        "3/6": "50%",
+        "4/6": "66.666667%",
+        "5/6": "83.333333%",
+        "1/12": "8.333333%",
+        "2/12": "16.666667%",
+        "3/12": "25%",
+        "4/12": "33.333333%",
+        "5/12": "41.666667%",
+        "6/12": "50%",
+        "7/12": "58.333333%",
+        "8/12": "66.666667%",
+        "9/12": "75%",
+        "10/12": "83.333333%",
+        "11/12": "91.666667%",
+        full: "100%",
+        screen: "100vw",
+        container: "100rem",
+      },
+      opacity: {
+        8: "0.08",
+      },
+      colors: {
+        "sky-blue-light": "#3c7aff",
+        "sky-blue-dark": "#0233bf",
+      },
+    },
   },
   variants: {},
-  plugins: [],
+  plugins: [require("@tailwindcss/ui")],
 };
